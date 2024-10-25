413c9a98bc460bdb87b0c4a15c09b744730cf2ed
Filmon Haile <hailef914@gmail.com>
2024-10-25 13:02:28 -0700

    more files

---
+++ Desktop/COND/contes.py
@@ -0,0 +1,24 @@
+"""
+mark=int(input("please Enter grade"))
+if mark>90:
+    grade="A"
+elif mark>80:
+    grade="B"
+elif mark>70:
+    grade="C"
+elif mark>60:
+    grade="D"
+else:
+    grade="F"
+    
+    print(grade)
+
+"""
+i=-3
+while i<10:
+  print(i)
+  if i==6:
+    break
+  i+=1
+
+     
